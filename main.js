dcb275a47fd79e84e11b69ff5e7273c8a6cea738
Dave Whelan <dwhelan2033@gmail.com>
2020-11-10 19:34:24 -0600

    init commit

---
+++ main.js
@@ -0,0 +1,113 @@
+
+// Logs a debug message
+let debug = false;
+let logDebug = (msg) => {	
+	if (debug) {
+		console.log(msg);
+	}
+};
+
+let hideResolvedMain = () => {
+
+	// Find the notes list
+	let el = document.getElementById("notes-list");
+
+	let toggleState = () => {
+		findResolved(el);
+		el.classList.toggle("hide-resolved");
+	};
+	
+	// Refresh the resolved notes
+	let findResolved = (updateClasses) => {
+		let notes = el.querySelectorAll(".note-discussion");
+		
+		logDebug(`Found ${notes.length} note-discussion elements`);
+		
+		let cnt = 0;
+		updateClasses = updateClasses ?? true;
+		for (let oneNote of notes) {
+			let resolved = oneNote.querySelector(".discussion-resolved-text");
+			if (resolved !== null) {
+				cnt++;
+			}
+			
+			if (updateClasses) {
+				oneNote.classList.toggle("hf-resolved", resolved !== null);
+			}
+		}
+		
+		updateResolvedCnt(cnt);
+		logDebug(`Found ${cnt} resolved discussions`);
+	};	
+
+	let updateResolvedCnt = (cnt) => {
+
+		if (lblToggleResolve.parentElement !== null) {
+			let resolveCntContainer = document.querySelector(".line-resolve-all-container");
+
+			if (resolveCntContainer !== null) {
+				resolveCntContainer.prepend(toggleContainer);
+			}
+		}
+
+		lblToggleResolve.innerText = `Show ${cnt} resolved`;	
+	};
+
+	if (el === null) {		
+		logDebug("Failed to find notes-list");
+		return;
+	}
+	
+	// Checkbox
+	let chkToggleResolve = document.createElement("input");
+	chkToggleResolve.type = "checkbox";
+	chkToggleResolve.id = "show-resolved-threads";
+	chkToggleResolve.addEventListener("change", toggleState);
+
+	// Label
+	let lblToggleResolve = document.createElement("label");
+	lblToggleResolve.innerText = `Show 0 resolved`;
+	lblToggleResolve.setAttribute("for", "show-resolved-threads");
+
+	// ToggleContainer
+	let toggleContainer = document.createElement("div");
+	toggleContainer.classList.add("line-resolve-all");
+	toggleContainer.appendChild(chkToggleResolve);
+	toggleContainer.appendChild(lblToggleResolve);
+	
+	logDebug("Found notes-list");
+
+	toggleState();  // starts in "hidden" state
+
+	// Options for the observer (which mutations to observe)
+	const config = { childList: true };
+
+	// Callback function to execute when mutations are observed
+	const callback = function(mutationsList, observer) {		
+		findResolved();
+	};
+
+	// Create an observer instance linked to the callback function
+	const observer = new MutationObserver(callback);
+
+	// Start observing the target node for configured mutations
+	observer.observe(el, config);
+
+	// Look for button clicks for resolve/unresolve
+	el.addEventListener("click", (ev) => {
+		var btn = ev.target.closest("button");
+
+		if (btn !== null) {
+			// Hack to account for the network delay
+			setTimeout(() => {				
+				var val = btn.dataset.qaSelector;
+				if (val === "resolve_discussion_button") {
+					findResolved(false);
+				}
+			}, 3000);
+		}
+		
+	});
+};
+
+hideResolvedMain();
